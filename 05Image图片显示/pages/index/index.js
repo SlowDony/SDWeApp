7c5047a12d804eb15abd036f571ab62897e09bde
slowdony <435574793@qq.com>
2017-10-22 23:39:10 +0800

    05Image图片显示

--- 05Image图片显示/pages/index/index.js
+++ 05Image图片显示/pages/index/index.js
@@ -3,10 +3,13 @@
 const app = getApp()
 
 Page({
+
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   }
+
 })
