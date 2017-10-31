c3c46a40b3c2cdf4811796c28c30e130d2b7ddf9
slowdony <435574793@qq.com>
2017-11-01 00:09:54 +0800

    08列表渲染

---
+++ 08列表渲染/pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    items : [
+        {"message":"view0"},
+        {"message":"view1"},
+        {"message":"view2"},
+        {"message":"view3"},
+        {"message":"view4"},
+    ]
+  }
+})
